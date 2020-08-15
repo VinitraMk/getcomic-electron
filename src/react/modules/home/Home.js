abcf2a00f0a2d3e3f12a593f022f4e61631af8de
VinitraMk <vinitramk@gmail.com>
2020-08-15 21:02:17 +0530

    added regex for url validation

--- src/react/modules/home/Home.js
+++ src/react/modules/home/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Loader from "../../components/loader/Loader";
+import {isUrlValid} from "../../utilities/UrlValidators";
 
 export default class Home extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             url:"",
-            showLoader:false
+            showLoader:false,
+            isUrlInvalid:false
         }
         this.searchForComics = this.searchForComics.bind(this);
         this.onUrlChange = this.onUrlChange.bind(this);
@@ -18,13 +20,36 @@ export default class Home extends React.Component {
             showLoader:true
         });
         e.preventDefault();
+
+        let res = isUrlValid(this.state.url);
+
+        if(res.isValid) {
+            console.log(res);
+            this.setState({
+                showLoader:false
+            })
+        }
+        else {
+            this.setState({
+                showLoader:false,
+                isUrlInvalid:true
+            })
+        }
     }
 
     onUrlChange(e) {
         //console.log(e.target.value);
-        this.setState({
-            url:e.target.value
-        })
+        if(e.target.value==="") {
+            this.setState({
+                isUrlInvalid:false,
+                url:e.target.value
+            })
+        }
+        else {
+            this.setState({
+                url:e.target.value
+            })
+        }
     }
 
     render() {
@@ -32,13 +57,14 @@ export default class Home extends React.Component {
             <div className="gc-container-ac">
                 <h1 className="gc-root-title">Getcomic</h1>
                 <form onSubmit={this.searchForComics}>
-                    <div className="gc-input-group">
+                    <div className={`gc-input-group ${this.state.isUrlInvalid ? "error-input" : ""}`}>
                         <input type="text" className="gc-input-control gc-input-control--search" 
                         placeholder="Enter series or comic issue url here..."
                         value={this.state.url}
                         onChange={this.onUrlChange}/>
                         <button type="submit" className="gc-btn gc-btn-primary">Search</button>
                     </div>
+                    {this.state.isUrlInvalid && <span className="error-message">The url is invalid!</span>}
                 </form>
                 {this.state.showLoader && <Loader message="Fetching Results"/>}
             </div>
